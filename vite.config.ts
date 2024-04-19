8d400743ad66a8176d59b9bd3f155ce3ba6fa5c8
g.k <gizensya.mail@gmail.com>
2024-04-19 09:13:43 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import build from "@hono/vite-cloudflare-pages";
-import devServer from "@hono/vite-dev-server";
-import adapter from "@hono/vite-dev-server/cloudflare";
 import { defineConfig } from "vite";
+import honox from "honox/vite";
+import client from "honox/vite/client";
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: "src/index.tsx",
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [client()],
+    };
+  } else {
+    return {
+      plugins: [honox(), build()],
+    };
+  }
 });
